12123d74799c02f978c37e1f4bd4cd4ee78b1972
romnoy <romano.noy2000@gmail.com>
2026-01-15 20:36:29 +0200

    Delete js directory

--- js/JSQ.js
+++
@@ -1,7 +0,0 @@
-// JSQ.js - Your original ML model file
-// TODO: Replace this with your actual JSQ.js file
-
-console.log('JSQ.js loaded - Please replace with your actual file');
-
-// This is a placeholder - your actual file should be here
-
